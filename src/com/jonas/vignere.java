88c6adeb3835186a0fdb366b194f8c8f25ed8fcd
mramazing01 <jonastoumazou@gmail.com>
2018-10-20 16:32:59 +0100

    Update vignere.java

--- src/com/jonas/vignere.java
+++ src/com/jonas/vignere.java
@@ -2,7 +2,7 @@ package com.jonas;
 
 import static java.lang.System.out;
 
-public class vignere {
+public class vignere extends encryption{
     protected void encryptCharacterNumber(int whichOne){
         out.println("test");
     }
