a3fd5e85187bfba6fa14a975b6c66fa6361d4622
jonas.toumazou <jonastoumazou@gmail.com>
2018-10-20 10:29:16 +0100

    1.1.5
    Signed-off-by: jonas.toumazou <jonastoumazou@gmail.com>

--- src/com/jonas/Main.java
+++ src/com/jonas/Main.java
@@ -6,10 +6,14 @@ public class Main {
         vernam ver = new vernam();
         ver.GetPlainText();
         ver.getKey();
+        String stringkey=ver.keytobin();
+        int newkey=Integer.parseInt(stringkey);
+        System.out.println(newkey);
+        /*
         ver.encryptCharacterNumber(1);
         encryption enc = new encryption();
         enc.GetPlainText();
         enc.getKey();
-        enc.encrypt();
+        enc.encrypt(); */
     }
 }
--- src/com/jonas/vernam.java
+++ src/com/jonas/vernam.java
@@ -3,10 +3,18 @@ package com.jonas;
 import static java.lang.System.out;
 
 public class vernam extends encryption{
+    public int newKey;
+
+    public String keytobin(){
+        Character p=key.toLowerCase().charAt(0);;
+        int m=(int)p;
+        String x=Integer.toBinaryString(m);
+        return x;
+    }
     protected void encryptCharacterNumber(int whichOne) {
         out.println(plaintext);
         out.println(key);
         out.println("This works");
-        /*I'm going to add some comments*/
+
     }
 }
